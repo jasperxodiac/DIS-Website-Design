a300bcbe289d2b340c0d3c20889bc2a17e5a8205
jasperxodiac <114020124+jasperxodiac@users.noreply.github.com>
2023-04-03 21:12:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Product Designer", "UI/UX Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
